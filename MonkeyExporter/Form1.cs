c80220ee8d228633b7dca77724180970d939dded
DESKTOP-IVEBHQV\Sparta <nbiresev@gmail.com>
2021-03-17 19:11:40 +0100

    add another 33% button

--- MonkeyExporter/Form1.cs
+++ MonkeyExporter/Form1.cs
@@ -31,7 +31,7 @@ namespace MonkeyExporter
         private void button1_Click(object sender, EventArgs e)
         {
             MouseOperations.handle = TableHandles.GetHandleWithTitle("MonkerSolver");
-            Task t = new Task(() => ClickOperatoins.OpenAllSolutions(270));
+            Task t = new Task(() => ClickOperatoins.OpenAllSolutions(121));
                 t.Start();
         }
 
